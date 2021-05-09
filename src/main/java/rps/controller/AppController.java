81e4f3cbd7a7f6e22009e4383fa186001b99db8e
jakeHacker99 <jakob.yaro@utb.ecutbildning.se>
2021-05-09 18:13:44 +0200

    Impl setName

--- src/main/java/rps/controller/AppController.java
+++ src/main/java/rps/controller/AppController.java
@@ -80,14 +80,11 @@ public class AppController {
         return  toGameDTO(gameService.makeMove(move,tokenId));
     }
 
-    @GetMapping(value = "/user/{name} ")
+    @GetMapping(value = "/user/{name}")
     public GameDTO setName(@PathVariable("name") String name,
                             @RequestHeader("token") String tokenId) {
         return  toGameDTO(gameService.setName(name, tokenId));
     }
-
-
-
 
 
    /*
